3d3858c8408017075e3f9ebdd034cbeb143ab6bb
da-moon <damoon.azarpazhooh@ryerson.ca>
2020-04-24 03:12:35 +0000

    core init

--- pkg/gossip/core/core.go
+++ pkg/gossip/core/core.go
@@ -1,1 +1,22 @@
 package core
+
+type Core interface {
+}
+type core struct {
+	// mutex , used for safeguarding struct
+	lock sync.Mutex
+	// is set to true when node is
+	// about to shut down
+	gracefulShutdown bool
+	// is set to true if node is shutdown
+	shutdownCh chan bool
+}
+
+// New creates a new gossip core
+func New() Core {
+	c := &core{
+		gracefulShutdown: false,
+		shutdownCh:       make(chan bool),
+	}
+	return c
+}
